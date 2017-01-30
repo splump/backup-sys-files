3322c2d68f794de24ef9d70ebde4eb2f7bf01c20
Daniel Andersson <github@muthur6000.se>
2017-01-30 21:14:15 +0100

    No commit msg on this one

---
+++ INSTALL.sh
@@ -0,0 +1,9 @@
+echo "Creating folder and copying config-file to /etc/backup-sys-files/filelist"
+mkdir -p /etc/backup-sys-files && cp ./filelist /etc/backup-sys-files/
+echo "Installing script to /usr/sbin/backup-sys-files.d"
+cp ./backup-sys-files.d /usr/sbin/
+echo "Installing systemd service file to /usr/lib/systemd/system/backup-sys-files.service"
+cp ./backup-sys-files.service /usr/lib/systemd/system/
+echo "Running systemctl daemon-reload"
+echo "systemctl daemon-reload"
+echo "Start the daemon by running: systemctl start backup-sys-files"
