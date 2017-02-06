634c9f965b653ec0bae2ab92ffdc2cae28482f92
Daniel Andersson <github@muthur6000.se>
2017-02-06 09:42:00 +0100

    Install script now installs restore.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -2,6 +2,8 @@ echo "Creating folder and copying config-file to /etc/backup-sys-files/filelist"
 mkdir -p /etc/backup-sys-files && cp ./filelist /etc/backup-sys-files/
 echo "Installing script to /usr/sbin/backup-sys-files.d"
 cp ./backup-sys-files.d /usr/sbin/
+echo "Installing restore script to /usr/sbin/restore.sh"
+cp ./restore.sh /usr/sbin/
 echo "Installing systemd service file to /usr/lib/systemd/system/backup-sys-files.service"
 cp ./backup-sys-files.service /usr/lib/systemd/system/
 echo "Running systemctl daemon-reload"
